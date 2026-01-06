c2acee98c7abc589575226ed20e1062d432d54e3
SUNG HYUNJOON <tjdguswns1941@gachon.ac.kr>
2026-01-07 02:01:29 +0900

    feat: Initialize Spring Boot project structure

---
+++ src/main/java/com/leets/monifit_be/MonifitBeApplication.java
@@ -0,0 +1,13 @@
+package com.leets.monifit_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonifitBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonifitBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leets/monifit_be/MonifitBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leets.monifit_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonifitBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
