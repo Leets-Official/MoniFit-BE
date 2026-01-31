7e8085457cec5c4f87e79c10c0bf45e5fd390641
seungjub270 <seungjub270@gmail.com>
2026-02-01 02:02:39 +0900

    Add ActiveProfiles annotation to test class

--- src/test/java/com/leets/monifit_be/MonifitBeApplicationTests.java
+++ src/test/java/com/leets/monifit_be/MonifitBeApplicationTests.java
@@ -2,13 +2,13 @@ package com.leets.monifit_be;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles; // ✅ 이거 필수
 
-@ActiveProfiles("test")
 @SpringBootTest
+@ActiveProfiles("test")
 class MonifitBeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
